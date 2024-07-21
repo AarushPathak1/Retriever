e552e600d2a46537db71fa8a64a97e6be1377afc
Aarush Pathak <apathak9@wisc.edu>
2024-07-21 14:27:39 +0400

    implemented a huge chunk of functionality of the MusicRecognizer.java class and a nested class structure in Song.java to appropriately store info from returned Json object

--- src/MusicRecognizer.java
+++ src/MusicRecognizer.java
@@ -1,8 +1,79 @@
-import java.util.List;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import com.google.gson.Gson;
 
+/**
+ * This class implements audio recognition capabilities for an input aufdio file.
+ * 
+ * @author aarushpathak
+ */
 public class MusicRecognizer {
+	
+	/** The private access token used to authenticate the API request */
+	private static final String API_TOKEN = "b0f392b9d4bfa9323ac50ef264cf954d";
+	/** URL of the song (where it can be accessed by the API) */ 
+	private String audioUrl;
 
-	public List<String> recognizeSongs(String audioFilePath){
+	/**
+	 * Recognizes the song that the inputted file plays.
+	 * 
+	 * @param audioUrl URL of the audio file
+	 * @return The name of the song being played
+	 */
+	public String recognizeSong(String audioUrl){
+		this.audioUrl = audioUrl;
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			Song song = new Song();
+			song.setUrl("https://github.com/AarushPathak1/Retriever/blob/main/fireball_trimmed.mp3?raw=true");
+			
+			// create the form body string with all necessary information
+			String formParams = "url=" + URLEncoder.encode(song.getUrl(), StandardCharsets.UTF_8)
+			+ "&return=" + URLEncoder.encode("spotify", StandardCharsets.UTF_8)
+			+ "&api_token=" + URLEncoder.encode(API_TOKEN, StandardCharsets.UTF_8);
+			
+			// create the Http request
+			HttpRequest postRequest = HttpRequest.newBuilder()
+					.uri(new URI("https://api.audd.io/"))
+					.header("Content-Type", "application/x-www-form-urlencoded")
+					.POST(BodyPublishers.ofString(formParams))
+					.build();
+			// send the Http request and record the response in the postResponse object
+			HttpClient client = HttpClient.newHttpClient();
+			HttpResponse<String> postResponse = client.send(postRequest, BodyHandlers.ofString());
+			
+			// Convert the json string to a Song object using Gson
+			Gson gson = new Gson();
+			song = gson.fromJson(postResponse.body(), Song.class);
+			
+			System.out.println(postResponse.body());
+			// print out results for tesing purposes
+			Song.Result result = song.getResult();
+            if (result != null) {
+                System.out.println("Title: " + result.getTitle());
+                System.out.println("Artist: " + result.getArtist());
+                System.out.println("Album: " + result.getAlbum());
+                if (result.getSpotify() != null && result.getSpotify().getTrack() != null) {
+                    System.out.println("Spotify ID: " + result.getSpotify().getTrack().getId());
+                }
+            } else {
+                System.out.println("No result found.");
+            }
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
---
+++ src/Song.java
@@ -0,0 +1,175 @@
+import com.google.gson.annotations.SerializedName;
+/**
+ * Represents a song object. When the conversion from a String to a json object
+ * is run in the MusicRecognizer class, appropriate parameters' values are stored
+ * in an instance of this class.
+ * 
+ * @author aarushpathak
+ */
+public class Song {
+	
+	/** URL of the song (where it can be accessed by the API) */
+	private String url;
+	/** the status of the request */
+	private String status;
+	/** The "result" sub section of the returned json object */
+    @SerializedName("result")
+    private Result result;
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	/**
+	 * @return result the result object
+	 */
+	public Result getResult() {
+        return result;
+    }
+	
+	/**
+	 * @param result
+	 */
+    public void setResult(Result result) {
+        this.result = result;
+    }
+	
+	
+	/** The "result" sub section of the returned json object */
+	public static class Result {
+		/** artist of the song */
+        @SerializedName("artist")
+        private String artist;
+        /** title of the song */
+        @SerializedName("title")
+        private String title;
+        /** name of the album the song is from */
+        @SerializedName("album")
+        private String album;
+        /** The "spotify" sub section of the returned json object */
+        @SerializedName("spotify")
+        private Spotify spotify;
+        
+    	/**
+    	 * @return the name of the artist
+    	 */
+        public String getArtist() {
+            return artist;
+        }
+
+    	/**
+    	 * @param artist the name of the artist
+    	 */
+        public void setArtist(String artist) {
+            this.artist = artist;
+        }
+
+    	/**
+    	 * @return the title of the song
+    	 */
+        public String getTitle() {
+            return title;
+        }
+
+    	/**
+    	 * @param title the title of the song
+    	 */
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+    	/**
+    	 * @return the album name
+    	 */
+        public String getAlbum() {
+            return album;
+        }
+
+    	/**
+    	 * @param album the name of the album
+    	 */
+        public void setAlbum(String album) {
+            this.album = album;
+        }
+
+    	/**
+    	 * @return the spotify object
+    	 */
+        public Spotify getSpotify() {
+            return spotify;
+        }
+
+    	/**
+    	 * @param spotify reference to the Spotify class
+    	 */
+        public void setSpotify(Spotify spotify) {
+            this.spotify = spotify;
+        }
+    }
+
+	/**
+	 * The "Spotify" sub section of the returned json object
+	 */
+    public static class Spotify {
+        @SerializedName("track")
+        private Track track;
+
+        /**
+         * @return track
+         */
+        public Track getTrack() {
+            return track;
+        }
+
+        /**
+         * @param track
+         */
+        public void setTrack(Track track) {
+            this.track = track;
+        }
+    }
+
+    public static class Track {
+    	/** The spotify_id of the song */
+        @SerializedName("id")
+        private String id;
+
+        /**
+         * @return the id
+         */
+        public String getId() {
+            return id;
+        }
+        
+        /**
+         * @param id the spotify_id of the song
+         */
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+}
